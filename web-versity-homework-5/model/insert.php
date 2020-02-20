9cdff489951e0735b788e057f4cbfbf5b7f59882
MD. ARIF KHAN <arifkhanshanto234@gmail.com>
2020-02-20 16:39:08 +0600

    some error update

--- web-versity-homework-5/model/insert.php
+++ web-versity-homework-5/model/insert.php
@@ -6,7 +6,7 @@ $email=$_POST['email'];
 $uname=$_POST['uname'];
 $phnnumber=$_POST['phnnumber'];
 $pass=md5(sha1($_POST['password']));
-$sql="INSERT INTO std_info (s_id, name, email, uname, password,phonenum) VALUES ('$sid', '$name', '$email','$uname', '$pass','$phnnumber')";
+$sql="INSERT INTO std_info (s_id, name, email, uname, password,phone_num) VALUES ('$sid', '$name', '$email','$uname', '$pass','$phnnumber')";
 
 $result=mysqli_query($myconn,$sql);
 if($result===TRUE)
